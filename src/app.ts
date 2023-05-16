e5394416872facbce36895a05be5b21ee5acd5ae
Radosław Kłos <radoslaw@klos.dev>
2023-05-16 23:05:33 +0200

    Configurable port

--- src/app.ts
+++ src/app.ts
@@ -70,4 +70,4 @@ app.all(`/${endpoint}/:path`, async (req, res) => {
     .send('OK');
 });
 
-app.listen(3000, () => console.log('Listening on port 3000!'));
+app.listen(process.env.PORT, () => console.log(`Listening on port ${process.env.PORT}!`));
