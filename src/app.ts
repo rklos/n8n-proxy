ecae0a3d3bf0dcb389ca66548bc18c26c694ef8f
Radosław Kłos <radoslaw@klos.dev>
2023-05-16 22:58:38 +0200

    Migration to axios + passing some headers

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,7 @@
+import axios from 'axios';
 import { config } from 'dotenv';
 import express from 'express';
 import rateLimit from 'express-rate-limit';
-import fetch from 'node-fetch';
 import proxyConfig from './config';
 
 config();
@@ -19,14 +19,16 @@ app.use(limiter);
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-const endpoint = process.env.ENDPOINT_PATH || '/';
-
 app.get('/test-ip', (req, res) => {
   res.status(200)
     .send(req.ip);
 });
 
-app.all(`${endpoint}:path`, async (req, res) => {
+const endpoint = (process.env.ENDPOINT_PATH || '')
+  .replace(/\/$/, '')
+  .replace(/^\//, '');
+
+app.all(`/${endpoint}/:path`, async (req, res) => {
   const proxyPath = req.params.path;
   const config = proxyConfig.find((item) => item.proxyPath === proxyPath);
   if (!config || config.method.toLowerCase() !== req.method.toLowerCase()) {
@@ -35,8 +37,23 @@ app.all(`${endpoint}:path`, async (req, res) => {
     return;
   }
 
-  const url = `${process.env.N8N_URL}/webhook${config.test ? '-test' : ''}/${config.webhookPath}`;
-  const response = await fetch(url, { method: req.method });
+  const n8nUrl = process.env.N8N_URL!.replace(/\/$/, '');
+
+  const url = `${n8nUrl}/webhook${config.test ? '-test' : ''}/${config.webhookPath}`;
+  const response = await axios({
+    url,
+    params: req.query,
+    headers: {
+      Accept: req.headers.Accept,
+      'user-agent': req.headers['user-agent'],
+      'accept-encoding': req.headers['accept-encoding'],
+      'Content-Type': req.headers['Content-Type'],
+    },
+    method: req.method,
+    data: req.body,
+    validateStatus: () => true,
+  });
+
   if (response.status !== 200) {
     res.status(500)
       .send('Internal Server Error');
@@ -44,9 +61,8 @@ app.all(`${endpoint}:path`, async (req, res) => {
   }
 
   if (config.returnResponse) {
-    const data = await response.json();
     res.status(200)
-      .send(data);
+      .send(response.data);
     return;
   }
 
